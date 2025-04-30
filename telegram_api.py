b1cab7ba49a65e58d433203e81d71b796894f6f2
Katerina <e.mayakovskaya@gmail.com>
2025-04-30 18:35:39 +0300

    Publish photo to Telegram channel

--- telegram_api.py
+++ telegram_api.py
@@ -5,4 +5,4 @@ tg_token = config('TG_TOKEN')
 bot = telegram.Bot(token=tg_token)
 
 chat_id = '@dvmn_space_photos'
-bot.send_message(chat_id=chat_id, text="Hello, world!")
+bot.send_photo(chat_id=chat_id, photo=open('images/spacex/spacex_0.jpg', 'rb'))
